4d6e506cf552c6ed863cee754fb09323b82c3aad
Daniel Fritzsche <daniel.fritzsche@bisdn.de>
2018-05-14 18:03:09 +0200

    get parameters + refactoring

--- Netconf2SoapMediator/src/main/java/com/technologies/highstreet/netconf2soapmediator/server/TR069Servlet.java
+++ Netconf2SoapMediator/src/main/java/com/technologies/highstreet/netconf2soapmediator/server/TR069Servlet.java
@@ -20,93 +20,114 @@ public class TR069Servlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 5071770086030271370L;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		System.out.println(TR069Servlet.getBody(request));
 		response.getWriter().println("Get Hello World!");
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		System.out.println("Received HTTP request:");
-		
+
 		final String reqBody = TR069Servlet.getBody(request);
 		System.out.println(reqBody);
-		
-	
-		if (reqBody.equals("")) {
-			System.out.println("Received HTTP request: Empty");
-			return;
-		}
-	
+
+		StringBuilder sb = new StringBuilder(10);
+		// Soap envelope
+		sb.append("<soapenv:Envelope ");
+		sb.append("xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" ");
+		sb.append("xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" ");
+		sb.append("xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" ");
+		sb.append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
+		sb.append("xmlns:cwmp=\"urn:dslforum-org:cwmp-1-0\">\n");
+		// header
+		sb.append("\t<soapenv:Header>\n");
+		sb.append("\t\t<cwmp:ID soapenv:mustUnderstand=\"1\">1</cwmp:ID>\n");
+		sb.append("\t</soapenv:Header>\n");
+
 		if (reqBody.contains("Fault")) {
 			System.out.println("Received Fault msg");
 			return;
 		}
-		
-		if (reqBody.contains("cwmp:Inform")) {
-
-			StringBuilder sb = new StringBuilder(10);
-			// Soap envelope
-			sb.append("<soapenv:Envelope ");
-			sb.append("xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" ");
-			sb.append("xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" ");
-			sb.append("xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" ");
-			sb.append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
-			sb.append("xmlns:cwmp=\"urn:dslforum-org:cwmp-1-0\">\n");
-			// header
-			sb.append("\t<soap_env:Header>\n");
-			sb.append("\t\t<cwmp:ID soapenv:mustUnderstand=\"1\">1</cwmp:ID>\n");
-			sb.append("\t</soap_env:Header>\n");
-			// body
-			sb.append("\t<soap_env:Body>\n");
-			sb.append("\t\t<cwmp:InformResponse>\n");
-			sb.append("\t\t\t<MaxEnvelopes>1</MaxEnvelopes>\n");
-			sb.append("\t\t</cwmp:InformResponse>\n");
-			sb.append("\t</soap_env:Body>\n");
-			// end
-			sb.append("</soapenv:Envelope>\n");
-			System.out.println("Sending HTTP reply:");
-			response.getWriter().println(sb);
+		else if (reqBody.contains("cwmp:Inform")) {
+			sendInform(sb);
 		}
+		else if (reqBody.equals("")) {
+			System.out.println("Received HTTP request: Empty");
+			//send GetParameterValues
+			System.out.println("Sending GetParameterValues msg");
+			sendGetParameterValues(sb);
+		}
+		else {return;}
+		
+		// end
+		sb.append("</soapenv:Envelope>\n");
+		System.out.println("Sending HTTP reply:");
+		System.out.println(sb);
+		response.getWriter().println(sb);
 	}
-	
-	
+
+	static StringBuilder sendInform(StringBuilder sb) {
+		// body
+		sb.append("\t<soapenv:Body>\n");
+		sb.append("\t\t<cwmp:InformResponse>\n");
+		sb.append("\t\t\t<MaxEnvelopes>1</MaxEnvelopes>\n");
+		sb.append("\t\t</cwmp:InformResponse>\n");
+		sb.append("\t</soapenv:Body>\n");
+
+		return sb;
+	}
+
+	static StringBuilder sendGetParameterValues(StringBuilder sb) {
+		// body
+		sb.append("\t<soapenv:Body>\n");
+		sb.append("\t\t<cwmp:GetParameterValues>\n");
+		sb.append("\t\t\t<ParameterNames soap:arrayType=\"xsd:string[2]\">\n");
+		sb.append("\t\t\t\t<string>Device.DeviceInfo.Manufacturer</string>\n");
+		sb.append("\t\t\t\t<string>Device.DeviceInfo.ProductClass</string>\n");
+		sb.append("\t\t\t</ParameterNames>\n");
+		sb.append("\t\t</cwmp:GetParameterValues>\n");
+		sb.append("\t</soapenv:Body>\n");
+
+		return sb;
+	}
+
 	public static String getBody(HttpServletRequest request) throws IOException {
 
-	    String body = null;
-	    StringBuilder stringBuilder = new StringBuilder();
-	    BufferedReader bufferedReader = null;
+		String body = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
 
-	    try {
-	        InputStream inputStream = request.getInputStream();
-	        if (inputStream != null) {
-	            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-	            char[] charBuffer = new char[128];
-	            int bytesRead = -1;
-	            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
-	                stringBuilder.append(charBuffer, 0, bytesRead);
-	            }
-	        } else {
-	            stringBuilder.append("");
-	        }
-	    } catch (IOException ex) {
-	        throw ex;
-	    } finally {
-	        if (bufferedReader != null) {
-	            try {
-	                bufferedReader.close();
-	            } catch (IOException ex) {
-	                throw ex;
-	            }
-	        }
-	    }
+		try {
+			InputStream inputStream = request.getInputStream();
+			if (inputStream != null) {
+				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+					stringBuilder.append(charBuffer, 0, bytesRead);
+				}
+			} else {
+				stringBuilder.append("");
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
 
-	    body = stringBuilder.toString();
-	    return body;
+		body = stringBuilder.toString();
+		return body;
 	}
 
 }
