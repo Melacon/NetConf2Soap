a1ed6bdb0930184288f2c44e17804ab11a317d69
Daniel Fritzsche <daniel.fritzsche@bisdn.de>
2018-05-22 14:18:58 +0200

    refactoring

--- Netconf2SoapMediator/src/main/java/com/technologies/highstreet/netconf2soapmediator/server/HTTPServlet.java
+++ Netconf2SoapMediator/src/main/java/com/technologies/highstreet/netconf2soapmediator/server/HTTPServlet.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -25,6 +24,7 @@ public class HTTPServlet extends HttpServlet {
 	private static final long serialVersionUID = 5071770086030271370L;
 	private static Netconf2SoapNetworkElement networkElement = null;
 	private static boolean setParam = true;
+	private static CWMPMessage CWMPmsg = new CWMPMessage();
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
@@ -41,8 +41,8 @@ public class HTTPServlet extends HttpServlet {
 		final String reqBody = HTTPServlet.getBody(request);
 		//System.out.println(reqBody);
 
-		StringBuilder sb  = generateString();
-
+		StringBuilder sb = new StringBuilder(10);
+		
 		if (reqBody.contains("Fault")) {
 			System.out.println("Received Fault msg");
 			return;
@@ -56,39 +56,37 @@ public class HTTPServlet extends HttpServlet {
 
 			setParam=true;
 			Netconf2SoapMediator.connActive = true;
-			System.out.println("Netconf2SoapMediator.connActive=" + Netconf2SoapMediator.connActive );
 			networkElement.setTr069DocumentCFromString(reqBody);
-			sendInform(sb);
+			sb = CWMPmsg.getInformResponse();
 		}
 		else if (reqBody.contains("cwmp:Inform") && reqBody.contains("<EventCode>2 PERIODIC")) {
 			System.out.println("Received Inform msg (PERIODIC REQUEST)");
 			Netconf2SoapMediator.connActive = true;
-			System.out.println("Netconf2SoapMediator.connActive=" + Netconf2SoapMediator.connActive );
 			networkElement.setTr069DocumentCFromString(reqBody);
-			sendInform(sb);
+			sb = CWMPmsg.getInformResponse();
 		}
 		else if (reqBody.contains("cwmp:Inform") && reqBody.contains("<EventCode>6 CONNECTION REQUEST")) {
 			System.out.println("Received Inform msg (CONNECTION REQUEST)");
 			Netconf2SoapMediator.connActive = true;
 			networkElement.setTr069DocumentCFromString(reqBody);
-			sendInform(sb);
+			sb = CWMPmsg.getInformResponse();
 		}
 		else if (reqBody.contains("cwmp:Inform") ) {
 			System.out.println("Received Inform msg (unknown)");
 			networkElement.setTr069DocumentCFromString(reqBody);
-			sendInform(sb);
+			sb = CWMPmsg.getInformResponse();
 		}
 		else if (reqBody.contains("cwmp:GetParameterValuesResponse")) {
 			System.out.println("Received GetParameterValuesResponse msg");
 			if (setParam == true) {
-				sendSetParameterValues(sb);
+				sb = CWMPmsg.setParameterValues();
 			} else {
-				sendGetParameterAttributes(sb);
+				sb = CWMPmsg.getParameterAttributes();
 			}
 		}
 		else if (reqBody.contains("cwmp:SetParameterValuesResponse")) {
 			System.out.println("Received SetParameterValuesResponse msg");
-			sendGetParameterValues(sb);
+			sb = CWMPmsg.getParameterValues();
 			setParam=false;
 		}
 		else if (reqBody.contains("cwmp:GetParameterAttributesResponse")) {
@@ -96,110 +94,20 @@ public class HTTPServlet extends HttpServlet {
 			// send empty response, close connection
 			//response.getWriter().println(sb2);
 			Netconf2SoapMediator.connActive = false;
-			System.out.println("Netconf2SoapMediator.connActive=" + Netconf2SoapMediator.connActive );
 			return;
 		}
 		else if (reqBody.equals("")) {
 			System.out.println("Received HTTP request: Empty");
 			//send GetParameterValues
-			sendGetParameterValues(sb);
+			sb = CWMPmsg.getParameterValues();
 		}
 		else {
 			System.out.println("Received Unknown msg");
 			return;}
 
-		// end
-		sb.append("</soapenv:Envelope>\n");
 		System.out.println("Sending HTTP reply:");
-		//System.out.println(sb);
+		System.out.println(sb);
 		response.getWriter().println(sb);
-	}
-
-	static StringBuilder sendInform(StringBuilder sb) {
-		System.out.println("Sending InformResponse msg");
-		// body
-		sb.append("\t<soapenv:Body>\n");
-		sb.append("\t\t<cwmp:InformResponse>\n");
-		sb.append("\t\t\t<MaxEnvelopes>1</MaxEnvelopes>\n");
-		sb.append("\t\t</cwmp:InformResponse>\n");
-		sb.append("\t</soapenv:Body>\n");
-
-		return sb;
-	}
-
-	static StringBuilder sendSetParameterValues(StringBuilder sb) {
-		Random rand = new Random(); 
-		int value = rand.nextInt(5000) + 1000;
-				
-		System.out.println("Sending SetParameterValues msg");
-		// body
-		sb.append("\t<soapenv:Body>\n");
-		sb.append("\t\t<cwmp:SetParameterValues>\n");
-		sb.append("\t\t\t<ParameterList soap:arrayType=\"cwmp:ParameterValueStruct[2]\">\n");
-		sb.append("\t\t\t<ParameterValueStruct>\n");
-		sb.append("\t\t\t\t<Name>Device.ManagementServer.PeriodicInformEnable</Name>\n");
-		sb.append("\t\t\t\t<Value>true</Value>\n");
-		sb.append("\t\t\t</ParameterValueStruct>\n");
-		sb.append("\t\t\t<ParameterValueStruct>\n");
-		sb.append("\t\t\t\t<Name>Device.ManagementServer.PeriodicInformInterval</Name>\n");
-		sb.append("\t\t\t\t<Value>" + value + "</Value>\n");
-		sb.append("\t\t\t</ParameterValueStruct>\n");
-		sb.append("\t\t\t</ParameterList>\n");
-		sb.append("\t\t\t<ParameterKey>12345</ParameterKey>\n");
-		sb.append("\t\t</cwmp:SetParameterValues>\n");
-		sb.append("\t</soapenv:Body>\n");
-
-		return sb;
-	}
-	
-	static StringBuilder generateString() {
-		StringBuilder sb = new StringBuilder(10);
-		// Soap envelope
-		//sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>");
-		sb.append("<soapenv:Envelope ");
-		sb.append("xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" ");
-		sb.append("xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" ");
-		sb.append("xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" ");
-		sb.append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
-		sb.append("xmlns:cwmp=\"urn:dslforum-org:cwmp-1-0\">\n");
-		// header
-		sb.append("\t<soapenv:Header>\n");
-		sb.append("\t\t<cwmp:ID soapenv:mustUnderstand=\"1\">1</cwmp:ID>\n");
-		sb.append("\t</soapenv:Header>\n");
-
-		return sb;
-	}
-
-	static StringBuilder sendGetParameterValues(StringBuilder sb) {
-		System.out.println("Sending GetParameterValues msg");
-		// body
-		sb.append("\t<soapenv:Body>\n");
-		sb.append("\t\t<cwmp:GetParameterValues>\n");
-		sb.append("\t\t\t<ParameterNames soap:arrayType=\"xsd:string[3]\">\n");
-		sb.append("\t\t\t\t<string>Device.DeviceInfo.UpTime</string>\n");
-		sb.append("\t\t\t\t<string>Device.ManagementServer.PeriodicInformEnable</string>\n");
-		sb.append("\t\t\t\t<string>Device.ManagementServer.PeriodicInformInterval</string>\n");
-		sb.append("\t\t\t</ParameterNames>\n");
-		sb.append("\t\t</cwmp:GetParameterValues>\n");
-		sb.append("\t</soapenv:Body>\n");
-
-		return sb;
-	}
-
-	static StringBuilder sendGetParameterAttributes(StringBuilder sb) {
-		System.out.println("Sending GetParameterAttributes msg");
-		// body
-		sb.append("\t<soapenv:Body>\n");
-		sb.append("\t\t<cwmp:GetParameterAttributes>\n");
-		sb.append("\t\t\t<ParameterNames soap:arrayType=\"xsd:string[3]\">\n");
-		sb.append("\t\t\t\t<string>Device.DeviceInfo.UpTime</string>\n");
-		sb.append("\t\t\t\t<string>Device.ManagementServer.PeriodicInformEnable</string>\n");
-		sb.append("\t\t\t\t<string>Device.ManagementServer.PeriodicInformInterval</string>\n");
-		sb.append("\t\t\t</ParameterNames>\n");
-		sb.append("\t\t</cwmp:GetParameterAttributes>\n");
-		sb.append("\t</soapenv:Body>\n");
-
-		return sb;
 	}
 
 	public static String getBody(HttpServletRequest request) throws IOException {
