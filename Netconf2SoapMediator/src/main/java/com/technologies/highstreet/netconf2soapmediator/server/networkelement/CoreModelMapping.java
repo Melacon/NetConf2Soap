063839c02d5c823fc67c707b4c9b3b2d4f5bd0d9
Daniel Fritzsche <daniel.fritzsche@bisdn.de>
2018-05-22 10:59:40 +0200

    fix hw version

--- Netconf2SoapMediator/src/main/java/com/technologies/highstreet/netconf2soapmediator/server/networkelement/CoreModelMapping.java
+++ Netconf2SoapMediator/src/main/java/com/technologies/highstreet/netconf2soapmediator/server/networkelement/CoreModelMapping.java
@@ -13,15 +13,15 @@ public class CoreModelMapping {
 		coreModelBiMap.put("Device.DeviceInfo.Manufacturer", "//data/equipment/manufactured-thing/manufacturer-properties/manufacturer-name"); // part of Inform RPC
 		coreModelBiMap.put("Device.DeviceInfo.ManufacturerOUI", "//data/equipment/manufactured-thing/manufacturer-properties/manufacturer-identifier%" +
 																"//data/equipment/uuid%" + 
-																"//data/equipment-pac/equipment%"+
+																"//data/equipment-pac/equipment%" +
 																"//data/network-element/uuid%" +
 																"//data/network-element/extension/value"); // part of Inform RPC
-		coreModelBiMap.put("Device.DeviceInfo.ModelName", "//data/equipment/manufactured-thing/manufacturer-properties/model-identifier%"+
-														  "//data/equipment/manufactured-thing/equipment-type/model-identifier"); 
+		coreModelBiMap.put("Device.DeviceInfo.ModelName", "//data/equipment/manufactured-thing/equipment-type/model-identifier"); 
 		coreModelBiMap.put("Device.DeviceInfo.Description", "//data/equipment/manufactured-thing/equipment-type/description");
 		coreModelBiMap.put("Device.DeviceInfo.ProductClass", "//data/equipment/manufactured-thing/equipment-type/part-type-identifier"); // part of Inform RPC
 		coreModelBiMap.put("Device.DeviceInfo.SerialNumber", "//data/equipment/manufactured-thing/equipment-instance/serial-number"); // part of Inform RPC
-		coreModelBiMap.put("Device.DeviceInfo.HardwareVersion", "//data/equipment/manufactured-thing/equipment-instance/asset-instance-identifier"); // part of Inform RPC
+		coreModelBiMap.put("Device.DeviceInfo.HardwareVersion", "//data/equipment/manufactured-thing/equipment-instance/asset-instance-identifier%" +
+				                                                "//data/equipment/manufactured-thing/equipment-type/version"); // part of Inform RPC
 		coreModelBiMap.put("Device.DeviceInfo.SoftwareVersion", "cmpac:software-version"); // part of Inform RPC
 		coreModelBiMap.put("Device.DeviceInfo.AdditionalHardwareVersion", "//data/equipment/manufactured-thing/operator-augmented-equipment-type/asset-instance-identifier");
 		coreModelBiMap.put("AdditionalSoftwareVersion", "cmpac:additional-software-version");
