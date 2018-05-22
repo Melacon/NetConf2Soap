0cae7d1ab9a2cc0e345003da77f57df89ead2af7
PierluigiGreto <gretopierluigi@gmail.com>
2018-05-22 14:55:33 +0200

    added new parameters to the network element

--- Netconf2SoapMediator/src/main/java/com/technologies/highstreet/netconf2soapmediator/server/networkelement/CoreModelMapping.java
+++ Netconf2SoapMediator/src/main/java/com/technologies/highstreet/netconf2soapmediator/server/networkelement/CoreModelMapping.java
@@ -15,22 +15,23 @@ public class CoreModelMapping {
 																"//data/equipment/uuid%" + 
 																"//data/equipment-pac/equipment%" +
 																"//data/network-element/uuid%" +
-																"//data/network-element/extension/value"); // part of Inform RPC
+																"//data/network-element/extension/value%" +
+																"//data/equipment-pac/equipment"); // part of Inform RPC
 		coreModelBiMap.put("Device.DeviceInfo.ModelName", "//data/equipment/manufactured-thing/equipment-type/model-identifier"); 
 		coreModelBiMap.put("Device.DeviceInfo.Description", "//data/equipment/manufactured-thing/equipment-type/description");
 		coreModelBiMap.put("Device.DeviceInfo.ProductClass", "//data/equipment/manufactured-thing/equipment-type/part-type-identifier"); // part of Inform RPC
 		coreModelBiMap.put("Device.DeviceInfo.SerialNumber", "//data/equipment/manufactured-thing/equipment-instance/serial-number"); // part of Inform RPC
 		coreModelBiMap.put("Device.DeviceInfo.HardwareVersion", "//data/equipment/manufactured-thing/equipment-instance/asset-instance-identifier%" +
 				                                                "//data/equipment/manufactured-thing/equipment-type/version"); // part of Inform RPC
-		coreModelBiMap.put("Device.DeviceInfo.SoftwareVersion", "cmpac:software-version"); // part of Inform RPC
+		coreModelBiMap.put("Device.DeviceInfo.SoftwareVersion", "//data/equipment-pac/equipment-status/software-version"); // part of Inform RPC
 		coreModelBiMap.put("Device.DeviceInfo.AdditionalHardwareVersion", "//data/equipment/manufactured-thing/operator-augmented-equipment-type/asset-instance-identifier");
-		coreModelBiMap.put("AdditionalSoftwareVersion", "cmpac:additional-software-version");
-		coreModelBiMap.put("UpTime", "cmpac:up-time");
+		coreModelBiMap.put("Device.DeviceInfo.AdditionalSoftwareVersion", "//data/equipment-pac/equipment-status/additional-software-version");
+		coreModelBiMap.put("Device.DeviceInfo.UpTime", "//data/equipment-pac/equipment-status/up-time");
 		coreModelBiMap.put("Device.DeviceInfo.FirstUseDate", "//data/equipment/manufactured-thing/equipment-instance/manufacture-date");
-		coreModelBiMap.put("Device.ManagementServer.ConnectionRequestURL", "cmpac:connection-request-url"); // part of Inform RPC
-		coreModelBiMap.put("ConnectionRequestUsername", "cmpac:connection-request-username");
-		coreModelBiMap.put("ConnectionRequestPassword", "cmpac:connection-request-password");
-		coreModelBiMap.put("LocalTimeZone", "cmpac:local-time-zone");
+		coreModelBiMap.put("Device.ManagementServer.ConnectionRequestURL", "//data/network-element-pac/network-element-status/connection-request-url"); // part of Inform RPC
+		coreModelBiMap.put("Device.DeviceInfo.ConnectionRequestUsername", "//data/network-element-pac/network-element-configuration/connection-request-username");
+		coreModelBiMap.put("Device.DeviceInfo.ConnectionRequestPassword", "//data/network-element-pac/network-element-configuration/connection-request-password");
+		coreModelBiMap.put("Device.DeviceInfo.LocalTimeZone", "//data/equipment-pac/equipment-status/local-time-zone");
 	}
 
 	static String getYangfromTR069(String tr069key ) {
